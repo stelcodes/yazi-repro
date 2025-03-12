2b854b76c388a9bab482097fd937e8e3234726d1
Stel Clementine <dev@stelclementine.com>
2025-03-12 10:54:34 -0700

    Tweak init.lua comment

--- init.lua
+++ init.lua
@@ -1,8 +1,7 @@
--- Testing header ----------------------------------------------------------------------
+-- Testing header (don't include this with your bug report) ----------------------
 Header:children_add(function(self)
   return "TESTING"
 end, 1000, Header.RIGHT)
-
 -- init.lua ----------------------------------------------------------------------
 
 
